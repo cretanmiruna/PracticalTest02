68da6e6ec482b9d208848829e51b3d3952367b7a
Student User <student@cti.pub.ro>
2018-05-25 13:19:59 +0300

    am terminat

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/general/Constants.java
@@ -0,0 +1,29 @@
+package ro.pub.cs.systems.eim.practicaltest02.general;
+
+public interface Constants {
+
+    final public static String TAG = "[PracticalTest02]";
+
+    final public static boolean DEBUG = true;
+
+    final public static String WEB_SERVICE_ADDRESS = "http://services.aonaware.com/CountCheatService/CountCheatService.asmx/LetterSolutionsMin?";
+
+    final public static String TEMPERATURE = "temperature";
+    final public static String WIND_SPEED = "wind_speed";
+    final public static String CONDITION = "condition";
+    final public static String PRESSURE = "pressure";
+    final public static String HUMIDITY = "humidity";
+    final public static String ALL = "all";
+
+    final public static String EMPTY_STRING = "";
+
+    final public static String QUERY_ATTRIBUTE1 = "anagram";
+    final public static String QUERY_ATTRIBUTE2 = "minLetters";
+
+    final public static String SCRIPT_TAG = "script";
+    final public static String STRING_TAG = "string";
+    final public static String SEARCH_KEY = "wui.api_data =\n";
+
+    final public static String CURRENT_OBSERVATION = "current_observation";
+
+}
---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/network/ClientThread.java
@@ -0,0 +1,79 @@
+package ro.pub.cs.systems.eim.practicaltest02.network;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.general.Utilities;
+
+public class ClientThread extends Thread {
+
+    private String address;
+    private int port;
+    private int nr_litere;
+    private String cuvant;
+    private TextView result_text_view;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String cuvant, int nr_litere, TextView result_text_view) {
+        this.address = address;
+        this.port = port;
+        this.nr_litere = nr_litere;
+        this.cuvant = cuvant;
+        this.result_text_view = result_text_view;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(address, port);
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+            printWriter.println(cuvant);
+            printWriter.flush();
+            printWriter.println(nr_litere);
+            printWriter.flush();
+            String weatherInformation;
+            while ((weatherInformation = bufferedReader.readLine()) != null) {
+                final String finalizedWeateherInformation = weatherInformation;
+                result_text_view.post(new Runnable() {
+                   @Override
+                    public void run() {
+                       result_text_view.setText(finalizedWeateherInformation);
+                   }
+                });
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
